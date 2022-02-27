00141d3eef857b29fb538800baefd56e75c28e59
Jean Pier Mobayed Kounboz <jeanmobayed@gmail.com>
2022-02-27 01:02:06 -0400

    feat: dockerizing the app

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,23 @@
 import express from "express";
 import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+import { MONGODB_URL } from "./config/db";
 
 const app = express();
 
-const PORT: string | number = process.env.PORT || 4000;
+const PORT: string | number = process.env.NODE_LOCAL_PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
 
-app.listen(PORT, () =>console.log(`Server running on http://localhost:${PORT}`));
+mongoose
+  .connect(MONGODB_URL)
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server running on http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => {
+    throw error;
+  });
